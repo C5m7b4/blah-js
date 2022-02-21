b8766d9272f4946d76d5effbc990cc5071d9f2e5
C5m7b4 <mbedingfield@dcrpos.com>
2022-02-21 16:22:19 -0600

    add toasts

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ window.addEventListener('onCategoriesLoaded', () => {
   runCategoryCode();
 });
 
+const toastContainer = document.createElement('div');
+toastContainer.id = 'toastContainer';
+toastContainer.classList.add('toast-container');
+document.body.appendChild(toastContainer);
+
 let data = [];
 let filteredData = data;
 
@@ -260,9 +265,9 @@ function runSampleCode() {
   const fruits = curriedFilter('fruit');
   const bevs = curriedFilter('beverages');
   const candy = curriedFilter('candy');
-  console.log('friuts', fruits);
-  console.log('bevs', bevs);
-  console.log('candy', candy);
+  createToast([...fruits], 'fruits');
+  createToast([...bevs], 'bevs');
+  createToast([...candy], 'candy');
 
   const findCategoryMostExpensiveItem = (array) => {
     return array.reduce((acc, cur) => {
@@ -296,3 +301,48 @@ function runCategoryCode() {
 
   createItemCategory();
 }
+
+const createToast = (text, title = '', duration = 4000, type) => {
+  const toastElem = document.createElement('div');
+  toastElem.classList.add('toast');
+  if (type) toastElem.classList.add(type);
+
+  const titleElem = document.createElement('p');
+  titleElem.classList.add('t-title');
+  titleElem.innerHTML = title;
+  toastElem.appendChild(titleElem);
+
+  const textElem = document.createElement('p');
+  textElem.classList.add('t-text');
+
+  if (Array.isArray(text)) {
+    const s = text
+      .map((s) => {
+        if (typeof s == 'object') {
+          return s.name;
+        } else {
+          return s;
+        }
+      })
+      .join(', ');
+    textElem.innerHTML = s;
+  } else {
+    textElem.innerHTML = text;
+  }
+
+  toastElem.appendChild(textElem);
+
+  const toastContainer = document.getElementById('toastContainer');
+  toastContainer.appendChild(toastElem);
+
+  setTimeout(() => {
+    toastElem.classList.add('active');
+  }, 1);
+
+  setTimeout(() => {
+    toastElem.classList.remove('active');
+    setTimeout(() => {
+      toastElem.remove();
+    }, 350);
+  }, duration);
+};
