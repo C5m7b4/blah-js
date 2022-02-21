f8bd8b52095510a52df52c6ae2bb3bec4824f4b4
C5m7b4 <mbedingfield@dcrpos.com>
2022-02-21 12:28:00 -0600

    add curried function

--- src/index.js
+++ src/index.js
@@ -109,3 +109,17 @@ const deleteItem = (id) => {
     buildTable();
   }
 };
+
+const filterData = (property) => {
+  return function (value) {
+    return data.filter((i) => i[property] == value);
+  };
+};
+
+const curriedFilter = filterData('category');
+const fruits = curriedFilter('fruit');
+const bevs = curriedFilter('beverages');
+const candy = curriedFilter('candy');
+console.log('friuts', fruits);
+console.log('bevs', bevs);
+console.log('candy', candy);
